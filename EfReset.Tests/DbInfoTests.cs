d3885acdd310661e66b57cb1f61d26a561668325
Rasyadi Rabuan <mrasyadi@gmail.com>
2019-07-09 04:19:18 +0800

    Add parse dbcontext info text to DbInfo object

---
+++ EfReset.Tests/DbInfoTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using System.Text;
+using Xunit;
+
+namespace EfReset.Tests
+{
+    public class DbInfoTests
+    {
+        [Theory, AutoData]
+        public void Parse_EmptyDbInfoText_ThrowsArgumentNullException(DbInfo sut)
+        {
+            Action act = () => sut.Parse(string.Empty);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory, AutoData]
+        public void Parse_InvalidDbInfoText_ReturnsDbInfo(string argument, DbInfo sut)
+        {
+            Action act = () => sut.Parse(argument);
+
+            act.Should().Throw<ArgumentException>().WithMessage("Invalid argument");
+        }
+
+        [Theory, AutoData]
+        public void Parse_ValidDbInfoText_ReturnsDbInfo(DbInfo expected, DbInfo sut)
+        {
+
+            var output = new StringBuilder();
+            output.Append("Provider name").Append(": ").Append(expected.ProviderName).Append("\r\n");
+            output.Append("Database name").Append(": ").Append(expected.DatabaseName).Append("\r\n");
+            output.Append("Data source").Append(": ").Append(expected.DataSource).Append("\r\n");
+            output.Append("options").Append(": ").Append(expected.Options).Append("\r\n");
+
+            var actual = sut.Parse(output.ToString());
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ EfReset/DbInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace EfReset
+{
+    public class DbInfo
+    {
+        public string ProviderName { get; set; }
+        public string DatabaseName { get; set; }
+        public string DataSource { get; set; }
+        public string Options { get; set; }
+
+        public DbInfo Parse(string output)
+        {
+            if (string.IsNullOrEmpty(output))
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (!output.Contains(":"))
+            {
+                throw new ArgumentException("Invalid argument");
+            }
+
+            var outputLines = output.Split("\r\n").Where(text => !string.IsNullOrEmpty(text));
+
+            foreach (var line in outputLines)
+            {
+                if (line.StartsWith("Provider name"))
+                {
+                    ProviderName = line.Split(":")[1].Trim();
+                }
+                else if (line.StartsWith("Database name"))
+                {
+                    DatabaseName = line.Split(":")[1].Trim();
+                }
+                else if (line.StartsWith("Data source"))
+                {
+                    DataSource = line.Split(":")[1].Trim();
+                }
+                else if (line.StartsWith("options"))
+                {
+                    Options = line.Split(":")[1].Trim();
+                }
+            }
+
+            return this;
+        }
+    }
+}
