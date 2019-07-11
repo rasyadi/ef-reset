dfc35fa037b43f1219ce037e326704b0d5533cdd
Rasyadi Rabuan <mrasyadi@gmail.com>
2019-07-11 10:04:11 +0800

    All capability to remove all tables and Migrations folder

--- EfReset/Migration.cs
+++ EfReset/Migration.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
 using System.IO.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EfReset
 {
@@ -18,9 +21,66 @@ namespace EfReset
 
         public void Remove(string projectPath)
         {
+            var dbContext = new DbContextInfo();
+            var dbInfo = new DbInfo();
+
             if (!_fileSystem.Directory.Exists(projectPath))
             {
                 throw new DirectoryNotFoundException(nameof(projectPath));
+            }
+
+            dbInfo = dbInfo.Parse(dbContext.GetInfo(projectPath));                       
+            
+            DropTables($"Data Source={Path.Combine(projectPath, dbInfo.DataSource)}");
+            Directory.Delete(Path.Combine(projectPath, "Migrations"), true);
+        }
+
+        private void DropTables(string connectionString)
+        {           
+            using (var context = new EfResetDbContext(connectionString))
+            {
+                var tables = new List<string>();
+                using (var connection = context.Database.GetDbConnection())
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT tbl_name FROM sqlite_master where type = 'table'";
+                        var reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            tables.Add(reader["tbl_name"].ToString());
+                        }                       
+                    }
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        foreach (var table in tables.Where(t => !t.Equals("sqlite_sequence")))
+                        {
+                            command.CommandText = $"DROP TABLE {table}";
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+        }
+
+        public class EfResetDbContext : DbContext
+        {
+            private readonly string _connectionString;
+
+            public EfResetDbContext(string connectionString)
+            {
+                _connectionString = connectionString;
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {                
+                optionsBuilder.UseSqlite(_connectionString);
             }
         }
     }
--- EfReset/Program.cs
+++ EfReset/Program.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EfReset
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            var dbContext = new DbContext();
-            Console.WriteLine(dbContext.GetInfo());
+        {          
+            var migration = new Migration();
+            migration.Remove(@"C:\\depot\\github\\ef-reset\\Sample.Ef");                       
+            
+            Console.WriteLine("The End");
         }
+
     }
 }
